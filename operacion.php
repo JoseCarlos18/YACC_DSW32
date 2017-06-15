5db5a5b708db8ad8459fbce37ba4e3557cec6099
JoseCarlos18 <josecarlos.gears@gmail.com>
2017-06-14 19:51:15 -0500

    operacion.PHP
    actualizacion del archivo PHP

--- operacion.php
+++ operacion.php
@@ -1,5 +1,20 @@
 <?php
 $n1=(isset($_REQUEST["n1"])?$_REQUEST["n1"]:0);
 $n2=(isset($_REQUEST["n2"])?$_REQUEST["n2"]:0);
-echo $n1+$n2;
+$op=(isset($_REQUEST['operacion'])?$_REQUEST['operacion']:null);
+
+if($op=='+'){
+	$re=$n1+$n2;
+} else {
+	if($op=='-'){
+	  $res=$sn1-$n2;
+	}elseif($op=='*'){
+	  $res=$sn1*$n2;
+	}elseif($op=='/'){
+	  $res=$sn1/$n2;
+	}else{
+	  $res="Operacion inválida";
+	}
+}
+echo $res;
 ?>
